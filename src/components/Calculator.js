e7bebe6f9ebf9e189e514e407ba2b1b45a1faaaf
Ben Kiboma <omayioben@gmail.com>
2022-06-29 18:44:22 +0300

    Style calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,27 +4,28 @@ function CalculatorApp() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">5555555555555</div>
-        <div className="current-operand">555555555555555</div>
+        <div className="previous-operand">0</div>
+        <div className="current-operand" />
       </div>
-      <button type="submit" className="span-two">AC</button>
-      <button type="submit">DEL</button>
-      <button type="submit">+</button>
-      <button type="submit">1</button>
-      <button type="submit">2</button>
-      <button type="submit">3</button>
-      <button type="submit">*</button>
-      <button type="submit">4</button>
-      <button type="submit">5</button>
-      <button type="submit">6</button>
-      <button type="submit">+</button>
+      <button type="submit">AC</button>
+      <button type="submit">+/-</button>
+      <button type="submit">%</button>
+      <button type="submit">/</button>
       <button type="submit">7</button>
       <button type="submit">8</button>
       <button type="submit">9</button>
+      <button type="submit">X</button>
+      <button type="submit">4</button>
+      <button type="submit">5</button>
+      <button type="submit">6</button>
       <button type="submit">-</button>
+      <button type="submit">1</button>
+      <button type="submit">2</button>
+      <button type="submit">3</button>
+      <button type="submit">+</button>
+      <button type="submit" className="span-two">0</button>
       <button type="submit">.</button>
-      <button type="submit">0</button>
-      <button type="submit" className="span-two">=</button>
+      <button type="submit">=</button>
     </div>
   );
 }
