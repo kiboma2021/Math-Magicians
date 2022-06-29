a381906eb43a46ddffcb8acf1ea16414f5a3d4c5
Ben Kiboma <omayioben@gmail.com>
2022-06-29 18:51:58 +0300

    Style buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,22 +10,22 @@ function CalculatorApp() {
       <button type="submit">AC</button>
       <button type="submit">+/-</button>
       <button type="submit">%</button>
-      <button type="submit">/</button>
+      <button type="submit" className="math-operands">/</button>
       <button type="submit">7</button>
       <button type="submit">8</button>
       <button type="submit">9</button>
-      <button type="submit">X</button>
+      <button type="submit" className="math-operands">X</button>
       <button type="submit">4</button>
       <button type="submit">5</button>
       <button type="submit">6</button>
-      <button type="submit">-</button>
+      <button type="submit" className="math-operands">-</button>
       <button type="submit">1</button>
       <button type="submit">2</button>
       <button type="submit">3</button>
-      <button type="submit">+</button>
+      <button type="submit" className="math-operands">+</button>
       <button type="submit" className="span-two">0</button>
       <button type="submit">.</button>
-      <button type="submit">=</button>
+      <button type="submit" className="math-operands">=</button>
     </div>
   );
 }
