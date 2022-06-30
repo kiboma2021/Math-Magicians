480954ac1a407c27d1ae21962a29e48813235081
Ben Kiboma <omayioben@gmail.com>
2022-06-30 21:52:10 +0300

    Add on click events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -32,24 +32,24 @@ class CalculatorApp extends Component {
           <div className="my-operand">{next ?? total ?? 0}</div>
         </div>
         <button type="submit" onClick={() => this.newOperation('AC')}>AC</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>+/-</button>
+        <button type="submit" onClick={() => this.newOperation('+/-')}>+/-</button>
         <button type="submit" onClick={() => this.newOperation('%')}>%</button>
         <button type="submit" onClick={() => this.newOperation('÷')} className="math-operands">÷</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>7</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>8</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>9</button>
-        <button type="submit" className="math-operands">X</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>4</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>5</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>6</button>
-        <button type="submit" onClick={() => this.newOperation('AC')} className="math-operands">-</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>1</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>2</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>3</button>
-        <button type="submit" onClick={() => this.newOperation('AC')} className="math-operands">+</button>
-        <button type="submit" onClick={() => this.newOperation('AC')} className="span-two">0</button>
-        <button type="submit" onClick={() => this.newOperation('AC')}>.</button>
-        <button type="submit" onClick={() => this.newOperation('AC')} className="math-operands">=</button>
+        <button type="submit" onClick={() => this.newOperation('7')}>7</button>
+        <button type="submit" onClick={() => this.newOperation('8')}>8</button>
+        <button type="submit" onClick={() => this.newOperation('9')}>9</button>
+        <button type="submit" onClick={() => this.newOperation('x')} className="math-operands">x</button>
+        <button type="submit" onClick={() => this.newOperation('4')}>4</button>
+        <button type="submit" onClick={() => this.newOperation('5')}>5</button>
+        <button type="submit" onClick={() => this.newOperation('6')}>6</button>
+        <button type="submit" onClick={() => this.newOperation('-')} className="math-operands">-</button>
+        <button type="submit" onClick={() => this.newOperation('1')}>1</button>
+        <button type="submit" onClick={() => this.newOperation('2')}>2</button>
+        <button type="submit" onClick={() => this.newOperation('3')}>3</button>
+        <button type="submit" onClick={() => this.newOperation('+')} className="math-operands">+</button>
+        <button type="submit" onClick={() => this.newOperation('0')} className="span-two">0</button>
+        <button type="submit" onClick={() => this.newOperation('.')}>.</button>
+        <button type="submit" onClick={() => this.newOperation('=')} className="math-operands">=</button>
       </div>
     );
   }
