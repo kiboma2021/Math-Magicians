f64269bb2a34415a820720d818439cf7d6cb581f
Ben Kiboma <omayioben@gmail.com>
2022-07-06 13:03:18 +0300

    Style Calculator and Quote page

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Quote = () => (
   <div>
-    <p className="quote">Mathematics is not about numbers, equations, computations or algorithms: it is about understanding. -William Paul Thurston</p>
+    <p className="quote">“Mathematics as an expression of the human mind reflects the active will, the contemplative reason, and the desire for aesthetic perfection. Its basic elements are logic and intuition, analysis and construction, generality and individuality.”— Richard Courant</p>
   </div>
 );
 export default Quote;
