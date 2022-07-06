5931921d4215fb09fea1256a315994b3f34fed3c
Ben Kiboma <omayioben@gmail.com>
2022-07-06 21:26:42 +0300

    Do unit testing using JEST

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import CalculatorApp from './Calculator';
 
-test('renders learn react link', () => {
-  render(<CalculatorApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test Calculator components', () => {
+  test('Check if it has button AC', () => {
+    render(<CalculatorApp />);
+    const linkElement = screen.getByText(/AC/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
